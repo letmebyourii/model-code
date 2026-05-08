d6be507c358bd77ae6b985c31478a1c2011da588
elukac20 <141880386+elukac20@users.noreply.github.com>
2026-05-07 21:45:25 -0400

    camera now rgb, model runs on videostream

--- video-stream.py
+++ video-stream.py
@@ -1,9 +1,13 @@
-# Code taken completely from the OpenCV tutorial
+# Code taken from the OpenCV tutorial
 # https://docs.opencv.org/4.x/dd/d43/tutorial_py_video_display.html
 
 import numpy as np
 import cv2 as cv
+from ultralytics import YOLO
  
+model = YOLO("yolov8n-oiv7.pt")
+
+
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
@@ -17,11 +21,21 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
     # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    color = cv.cvtColor(frame, cv.COLOR_RGB2RGBA)
     # Display the resulting frame
-    cv.imshow('frame', gray)
+    cv.imshow('frame', color)
     if cv.waitKey(1) == ord('q'):
         break
+
+    detection = model.predict(source='0', show=True)
+
+
+
+
+
+
+
+
  
 # When everything done, release the capture
 cap.release()
